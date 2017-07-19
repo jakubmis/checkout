8f0babb3a6760588de98aac5a7a0df160a6c5ca4
Mis <jakubmis91@gmail.com>
2017-07-19 22:20:59 +0200

    #1 spring boot project

---
+++ src/main/java/com/pc/checkout/Main.java
@@ -0,0 +1,15 @@
+package com.pc.checkout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Mis on 2017-07-19.
+ */
+@SpringBootApplication
+public class Main {
+    public static void main( String[] args )
+    {
+        SpringApplication.run(Main.class, args);
+    }
+}
