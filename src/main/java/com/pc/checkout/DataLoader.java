d029320b0fde9829c72982557fcc38a4031759c6
Mis <jakubmis91@gmail.com>
2017-07-23 16:21:00 +0200

    #4 test fix + readme

--- src/main/java/com/pc/checkout/DataLoader.java
+++ src/main/java/com/pc/checkout/DataLoader.java
@@ -1,7 +1,6 @@
 package com.pc.checkout;
 
 import com.pc.checkout.domain.AuthenticationDomain;
-import com.pc.checkout.domain.CheckoutDomain;
 import com.pc.checkout.persistence.entities.Customer;
 import com.pc.checkout.persistence.entities.Item;
 import com.pc.checkout.persistence.entities.ItemPromotion;
@@ -9,8 +8,6 @@ import com.pc.checkout.persistence.entities.Rebate;
 import com.pc.checkout.persistence.repositories.CustomerRepository;
 import com.pc.checkout.persistence.repositories.ItemRepository;
 import com.pc.checkout.persistence.repositories.RebateRepository;
-import com.pc.checkout.utils.Response;
-import com.pc.checkout.utils.Token;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.stereotype.Component;
@@ -26,38 +23,46 @@ import static org.hibernate.validator.internal.util.CollectionHelper.asSet;
 public class DataLoader {
 
     @Autowired
-    ConfigurableApplicationContext context;
+    private ConfigurableApplicationContext context;
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    private AuthenticationDomain authenticationDomain;
+
+    @Autowired
+    private ItemRepository itemRepository;
+
+    @Autowired
+    private RebateRepository rebateRepository;
 
     @PostConstruct
     public void init() {
-        CustomerRepository customerRepository = context.getBean(CustomerRepository.class);
-        AuthenticationDomain authenticationDomain = context.getBean(AuthenticationDomain.class);
+        createUsers();
+        createItemsAnPromotions();
+    }
+
+    private void createUsers() {
         customerRepository.save(new Customer("ADMIN"));
+        customerRepository.save(new Customer("MACIEJ"));
+        customerRepository.save(new Customer("TOMEK"));
+    }
 
-        Response<Token> login = authenticationDomain.login("ADMIN");
-
-        Item a = new Item(6, "A");
-
+    private void createItemsAnPromotions() {
+        Item a = new Item(5, "APPLE");
         a.setPromotion(new ItemPromotion(5, 10));
+        Item b = new Item(2, "BANANA");
+        Item c = new Item(100, "CHERRY");
+        c.setPromotion(new ItemPromotion(2, 80));
+        Item d = new Item(10, "DILL");
 
-        Item b = new Item(2, "B");
-        Item c = new Item(100, "C");
-        Item d = new Item(10, "D");
-
-        ItemRepository itemRepository = context.getBean(ItemRepository.class);
         itemRepository.save(a);
         itemRepository.save(b);
         itemRepository.save(c);
         itemRepository.save(d);
 
-        RebateRepository rebateRepository = context.getBean(RebateRepository.class);
-        rebateRepository.save(new Rebate(10, asSet(a, b, c, d)));
-        rebateRepository.save(new Rebate(100, asSet(a, b, c)));
-
-        CheckoutDomain checkoutDomain = context.getBean(CheckoutDomain.class);
-        checkoutDomain.putItemToBasket(login.getData(), "A", 10);
-        checkoutDomain.putItemToBasket(login.getData(), "B", 10);
-        checkoutDomain.putItemToBasket(login.getData(), "C", 10);
-
+        rebateRepository.save(new Rebate(50, asSet(a, b, c, d)));
+        rebateRepository.save(new Rebate(10, asSet(a, b, c)));
     }
 }
--- src/test/java/com/pc/checkout/web/controllers/CheckoutControllerTest.java
+++ src/test/java/com/pc/checkout/web/controllers/CheckoutControllerTest.java
@@ -3,10 +3,13 @@ package com.pc.checkout.web.controllers;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.pc.checkout.Main;
+import com.pc.checkout.domain.CheckoutDomain;
+import com.pc.checkout.utils.Token;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -16,6 +19,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 /**
@@ -27,12 +31,19 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 @ContextConfiguration(classes = Main.class)
 public class CheckoutControllerTest {
 
+    final private static String RECEIPT_HEADER = "Receipt\n------------\n";
+    final private static String RECEIPT_FOOTER = "------------\n" + "Price: 440\n" + "Rebate granted: 50\n" + "Total price: 390";
+    final private static String RECEIPT_CONTENT_APPLE = "|Product: APPLE|Amount: 10|Price: 50|AmountOfPromotion: 2|Promotion: 30|Total: 20\n";
+    final private static String RECEIPT_CONTENT_BANANA = "|Product: BANANA|Amount: 10|Price: 20|AmountOfPromotion: 0|Promotion: 0|Total: 20\n";
+    final private static String RECEIPT_CONTENT_CHERRY = "|Product: CHERRY|Amount: 10|Price: 1000|AmountOfPromotion: 5|Promotion: 600|Total: 400";
+
+
+    @Autowired
+    private ConfigurableApplicationContext context;
     @Autowired
     private CheckoutController checkoutController;
-
     @Autowired
     private AuthenticationController authenticationController;
-
     private MockMvc mockMvc;
     private String token;
     private ObjectMapper mapper = new ObjectMapper();
@@ -50,7 +61,7 @@ public class CheckoutControllerTest {
     public void testAddItemToBasket() throws Exception {
         MvcResult mvcResult = mockMvc
                 .perform(post("/checkout/item/add")
-                        .param("name", "A")
+                        .param("name", "APPLE")
                         .param("amount", "5")
                         .header("Authorization", token))
                 .andReturn();
@@ -62,27 +73,40 @@ public class CheckoutControllerTest {
 
     @Test
     public void testScan() throws Exception {
+        generateBasket();
         MvcResult mvcResult = mockMvc
-                .perform(post("/checkout/item/scan")
+                .perform(get("/checkout/item/scan")
                         .header("Authorization", token))
                 .andReturn();
         String receipt = mvcResult.getResponse().getContentAsString();
 
         assertNotNull(receipt);
+        assertTrue(receipt.contains(RECEIPT_HEADER));
+        assertTrue(receipt.contains(RECEIPT_FOOTER));
+        assertTrue(receipt.contains(RECEIPT_CONTENT_APPLE));
+        assertTrue(receipt.contains(RECEIPT_CONTENT_BANANA));
+        assertTrue(receipt.contains(RECEIPT_CONTENT_CHERRY));
     }
 
     @Test
     public void testPay() throws Exception {
+        generateBasket();
         MvcResult mvcResult = mockMvc
                 .perform(post("/checkout/item/pay")
                         .header("Authorization", token))
                 .andReturn();
         String receipt = mvcResult.getResponse().getContentAsString();
 
+
         assertNotNull(receipt);
+        assertTrue(receipt.contains(RECEIPT_HEADER));
+        assertTrue(receipt.contains(RECEIPT_FOOTER));
+        assertTrue(receipt.contains(RECEIPT_CONTENT_APPLE));
+        assertTrue(receipt.contains(RECEIPT_CONTENT_BANANA));
+        assertTrue(receipt.contains(RECEIPT_CONTENT_CHERRY));
     }
 
-    public void generateToken() throws Exception {
+    private void generateToken() throws Exception {
         MockMvc tokenMVC = MockMvcBuilders
                 .standaloneSetup(authenticationController)
                 .build();
@@ -93,5 +117,12 @@ public class CheckoutControllerTest {
         String json = mvcResult.getResponse().getContentAsString();
         final ObjectNode node = mapper.readValue(json, ObjectNode.class);
         token = node.get("token").asText();
+    }
+
+    private void generateBasket() throws Exception {
+        CheckoutDomain checkoutDomain = context.getBean(CheckoutDomain.class);
+        checkoutDomain.putItemToBasket(new Token(token), "APPLE", 10);
+        checkoutDomain.putItemToBasket(new Token(token), "BANANA", 10);
+        checkoutDomain.putItemToBasket(new Token(token), "CHERRY", 10);
     }
 }
